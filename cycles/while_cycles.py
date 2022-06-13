9ce130fd49a6c2a05d94599739ed2f871b0be249
1tuz <creameanwonder@gmail.com>
2022-06-13 17:35:18 +0300

    Start

---
+++ cycles/while_cycles.py
@@ -0,0 +1,12 @@
+# while условное_выражение:
+#   инструкции
+
+# пример :
+number = 1
+
+while number < 5:
+    print(f"number = {number}")
+    number += 1
+else:  # опционально
+    print(f"number = {number}. Работа цикла завершена")
+print("Работа программы завершена")
