a937c1edbe05461f6602b6992e245b56d38f41f7
1tuz <creameanwonder@gmail.com>
2022-07-10 13:41:12 +0300

    Added venv + some files

---
+++ functions/func.py
@@ -0,0 +1,5 @@
+def say_hello():  # определение функции say_hello
+    print("Hello")
+
+
+say_hello()  # вызов функции say_hello
---
+++ venv_variations/pip.py
@@ -0,0 +1,5 @@
+pip install -r requierments.txt # Устанавливает зависимости
+pip freeze > requirements.txt # Создание зависимостей
+pip install package-name # устанавливает последнюю версию пакета;
+pip uninstall package-name # Удаление пакета
+
