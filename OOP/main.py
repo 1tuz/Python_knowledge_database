dae724cd0124fbd979b978d12b7a8d5328e56c69
1tuz <creameanwonder@gmail.com>
2023-02-02 22:08:38 +0300

    something has been added

---
+++ OOP/main.py
@@ -0,0 +1,7 @@
+from my_module import MyClass
+
+
+obj = MyClass(5)
+obj.add_one()
+result = obj.get_x()
+print(result) # 6
---
+++ OOP/my_module.py
@@ -0,0 +1,9 @@
+class MyClass:
+    def __init__(self, x):
+        self.x = x
+
+    def add_one(self):
+        self.x += 1
+
+    def get_x(self):
+        return self.x
